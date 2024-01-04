ded8bd746d8a9b8c1bfb4e50f8ac02e7d021ad70
MAC002 <mac002@MAC002s-Mac-mini.local>
2024-01-04 17:30:26 +0700

    Task -1 | error component live

---
+++ src/common/svg_common.tsx
@@ -0,0 +1,12 @@
+// import { View } from "react-native"
+// import {  Svg } from "react-native-svg"
+
+// function SVGCommon  (){
+//     const SVGCom = require('../assets/images/live_logo')
+// return(
+//     <View style={{height:300, width:200}} >
+//      <Image source={SVGCom} alt="svg"></Image>
+//     </View>
+// )
+// }
+// export default SVGCommon
--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -2,23 +2,28 @@ import {Button} from '@ui-kitten/components';
 import {StyleSheet, Text, View} from 'react-native';
 import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
 
-
 function Login({navigation}: any) {
   const rnBiometrics = new ReactNativeBiometrics();
   async function isSensorAvailable() {
     const isDensorAvailable = await rnBiometrics.isSensorAvailable();
     console.log(isDensorAvailable);
+    //ios "Error Domain=com.apple.LocalAuthentication Code=-7 \"No identities are enrolled.\"
+    // "UserInfo={NSDebugDescription=No identities are enrolled., NSLocalizedDescription=Biometry is not enrolled."
+    //không cấp quyền : "Error Domain=com.apple.LocalAuthentication Code=-6 \"User has denied the use of biometry for this app.\"->
+    //UserInfo={NSDebugDescription=User has denied the use of biometry for this app., NSLocalizedDescription=Biometry is not available.}"}
   }
 
   async function createKeys() {
-   try {
-    const createKeys = await rnBiometrics.createKeys();
-    console.log(createKeys);
-   } catch (error) {
-    // Error generating public private keys
-    console.log('error createKeys:', error.message)
-   }
-    
+    try {
+      const createKeys = await rnBiometrics.createKeys();
+      console.log(createKeys);
+    } catch (error: any) {
+      // Error generating public private keys
+      //ios
+      //off biometric
+      //-Key generation error: Error Domain=NSOSStatusErrorDomain Code=-25293 "Key generation failed, error -25293" UserInfo={numberOfErrorsDeep=0, NSDescription=Key generation failed, error -25293}
+      console.log('error createKeys:', error.message);
+    }
   }
 
   async function biometricKeysExist() {
@@ -32,57 +37,73 @@ function Login({navigation}: any) {
   }
 
   async function createSignature() {
-   try {
-   
-    let payload ='some message';
-    const createSignature = await rnBiometrics.createSignature({
-      promptMessage: 'Sign in',
-      payload: payload,
-    });
-    console.log(createSignature);
-   } catch (error) {
-    // Error generating signature: No installed provider supports this key: (null)
-    // Error generating signature: Key permanently invalidated
-    console.log('error createsignature:',error.message)
-   }
-    
+    try {
+      let payload = 'some message';
+      const createSignature = await rnBiometrics.createSignature({
+        promptMessage: 'Sign in',
+        payload: payload,
+      });
+      console.log(createSignature);
+    } catch (error) {
+      // Error generating signature: No installed provider supports this key: (null)
+      // Error generating signature: Key permanently invalidated
+
+      //ios
+      //Key not found: error item not found
+      //Key not found: error item authentication failed
+      console.log('error createsignature:', error.message);
+    }
   }
 
- async function simplePrompt() {
-   try {
-    const simplePrompt = await rnBiometrics.simplePrompt({
-      promptMessage: 'Sign in',
-    });
-    console.log(simplePrompt);
-   } catch (error) {
-    // No fingerprints enrolled.
-    console.log('error simple:',error.message)
-   }
-  
+  async function simplePrompt() {
+    try {
+      const simplePrompt = await rnBiometrics.simplePrompt({
+        promptMessage: 'Sign in',
+      });
+      console.log(simplePrompt);
+    } catch (error) {
+      // No fingerprints enrolled.
+      //ios Error Domain=com.apple.LocalAuthentication Code=-7 "No identities are enrolled." UserInfo={NSDebugDescription=No identities are enrolled., NSLocalizedDescription=Biometry is not enrolled.
+      //không cấp quyền:  Error Domain=com.apple.LocalAuthentication Code=-6 "User has denied the use of biometry for this app." UserInfo={NSDebugDescription=User has denied the use of biometry for this app., NSLocalizedDescription=Biometry is not available.}
+      console.log('error simple:', error.message);
+    }
   }
 
   function recoverScreens(): void {
-    navigation.navigate('Screens-3')  }
+    navigation.navigate('Screens-3');
+  }
 
   return (
-    <View style={{flex:1,justifyContent:'space-between'}}>
-      <Button style={styles.box} onPress={() => isSensorAvailable()}>isSensorAvailable</Button>
-      <Button style={styles.box} onPress={() => createKeys()}>createKeys</Button>
-      <Button style={styles.box} onPress={() => biometricKeysExist()}>biometricKeysExist</Button>
-      <Button style={styles.box} onPress={() => deleteKeys()}>deleteKeys</Button>
-      <Button style={styles.box} onPress={() => createSignature()}>createSignature</Button>
-      <Button style={styles.box} onPress={() => simplePrompt()}>simplePrompt</Button>
-      <Button style={styles.box} onPress={() => recoverScreens()}> Screens 003</Button>
-  
+    <View style={{flex: 1, justifyContent: 'space-between'}}>
+      <Button style={styles.box} onPress={() => isSensorAvailable()}>
+        isSensorAvailable
+      </Button>
+      <Button style={styles.box} onPress={() => createKeys()}>
+        createKeys
+      </Button>
+      <Button style={styles.box} onPress={() => biometricKeysExist()}>
+        biometricKeysExist
+      </Button>
+      <Button style={styles.box} onPress={() => deleteKeys()}>
+        deleteKeys
+      </Button>
+      <Button style={styles.box} onPress={() => createSignature()}>
+        createSignature
+      </Button>
+      <Button style={styles.box} onPress={() => simplePrompt()}>
+        simplePrompt
+      </Button>
+      <Button style={styles.box} onPress={() => recoverScreens()}>
+        Screens 003
+      </Button>
     </View>
   );
 }
 export default Login;
 
 const styles = StyleSheet.create({
-
-    box: {
-      width: 200,
-      height: 50,
-    }
-})
+  box: {
+    width: 200,
+    height: 50,
+  },
+});
